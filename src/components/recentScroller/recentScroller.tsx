12ef025d652c09b7e60d55e43bc664bd7ae7acb5
Jayson Tian <48577877+Jaysontian@users.noreply.github.com>
2024-02-19 01:41:19 -0800

    fixed string type error

--- src/components/recentScroller/recentScroller.tsx
+++ src/components/recentScroller/recentScroller.tsx
@@ -52,7 +52,7 @@ export default async function RecentScroller(/*{}: Props*/) {
                 {uniqdata.map((post, index) => (
                 <CarouselItem key={index} className=" basis-1/3 md:basis-1/2 lg:basis-1/3">
                     <div className="pr-0.2">
-                        <CandidateBox name={post.applicant.profile?.name} candidateID={post.applicant.uid}>
+                        <CandidateBox name={post.applicant.profile?.name as string} candidateID={post.applicant.uid as string}>
                             <Card className="bg-zinc-700/50 text-white border-zinc-600 cursor-pointer hover:border-zinc-400">
                                 <CardContent className="flex flex-col items-left justify-center py-4 px-2">
                                     <span className="text-sm font-semibold">{post.applicant.profile?.name}</span>
